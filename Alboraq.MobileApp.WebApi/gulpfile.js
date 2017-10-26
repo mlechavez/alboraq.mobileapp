71e80c100699ece2e8965f56c3e7c8f112dafc1a
Mark Lester Echavez <lester.echavez@gmail.com>
2017-10-26 21:53:30 +0300

    added front end dependencies

---
+++ Alboraq.MobileApp.WebApi/gulpfile.js
@@ -0,0 +1,50 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),    
+    sass = require('gulp-sass'),
+    notify = require('gulp-notify'),
+    bower = require('gulp-bower');
+
+var config = {
+    sassPath: './assets/sass',
+    bowerDir: './bower_components',    
+};
+
+gulp.task('bower', function () {
+    return bower().pipe(gulp.dest(config.bowerDir));
+});
+
+gulp.task('icons', function () {
+    return gulp.src(config.bowerDir + '/font-awesome/fonts/**.*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+gulp.task('js', function () {
+    return gulp.src([
+        config.bowerDir + '/jquery/dist/jquery.min.js',
+        config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.min.js'
+    ]).pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('css', function () {
+    return gulp.src(config.sassPath + '/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths: [
+                './assets/sass',
+                config.bowerDir + './bootstrap-sass/assets/stylesheets',
+                config.bowerDir + './font-awesome/scss'
+            ]
+        }).on('error', notify.onError(function (error) {
+                return 'Error: ' + error.message;
+            })))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['bower', 'icons', 'css']);
