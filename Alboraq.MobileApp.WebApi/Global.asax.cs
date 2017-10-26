aaef1761e54e411079bd807e6c3a53aebc397a44
Mark Lester Echavez <lester.echavez@gmail.com>
2017-10-26 22:02:01 +0300

    removed bundleconfig

--- Alboraq.MobileApp.WebApi/Global.asax.cs
+++ Alboraq.MobileApp.WebApi/Global.asax.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Alboraq.MobileApp.WebApi
@@ -17,8 +12,7 @@ namespace Alboraq.MobileApp.WebApi
             UnityConfig.RegisterComponents();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
     }
 }
