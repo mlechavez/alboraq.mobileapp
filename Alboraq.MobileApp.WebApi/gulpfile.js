d9255c08610f21487f30ff3a5e26df9cc911fe47
Mark Lester Echavez <lester.echavez@gmail.com>
2017-10-28 11:22:35 +0300

    rebuilding node sass

--- Alboraq.MobileApp.WebApi/gulpfile.js
+++ Alboraq.MobileApp.WebApi/gulpfile.js
@@ -3,14 +3,14 @@ This file is the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp'),    
+var gulp = require('gulp'),
     sass = require('gulp-sass'),
     notify = require('gulp-notify'),
-    bower = require('gulp-bower');
+    bower = require('gulp-bower');    
 
 var config = {
     sassPath: './assets/sass',
-    bowerDir: './bower_components',    
+    bowerDir: './bower_components',
 };
 
 gulp.task('bower', function () {
@@ -38,8 +38,8 @@ gulp.task('css', function () {
                 config.bowerDir + './font-awesome/scss'
             ]
         }).on('error', notify.onError(function (error) {
-                return 'Error: ' + error.message;
-            })))
+            return 'Error: ' + error.message;
+        })))
         .pipe(gulp.dest('./dist/css'));
 });
 
