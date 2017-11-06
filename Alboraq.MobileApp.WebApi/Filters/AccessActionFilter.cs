1493805003de4416ccd596ec61a018544e13fe93
Mark Lester Echavez <lester.echavez@gmail.com>
2017-11-06 15:57:08 +0300

    added access action filter

---
+++ Alboraq.MobileApp.WebApi/Filters/AccessActionFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Alboraq.MobileApp.WebApi.Filters
+{
+    public class AccessActionFilter : ActionFilterAttribute
+    {
+        public string RoleName { get; set; }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(!filterContext.HttpContext.User.IsInRole(RoleName))
+            {
+                filterContext.Result = new ViewResult { ViewName = "_UnAuthorized" };
+            }
+        }
+    }
+}
